b3aa8936d3bca1fe211f3a78144bd891af0f7b63
WuJXcoder <coderwujx@gmail.com>
2023-03-20 21:49:57 +0800

    封装hash含防护

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# typescript 和 JavaScript 实现数据结构
+# typescript 和 部分使用 JavaScript 实现数据结构
 
 按照数据目录区分
---
+++ hashTable/哈希函数/hashFunction.ts
@@ -0,0 +1,21 @@
+/**
+ * 哈希函数，将key转换为index
+ * @author coderwujx
+ * @param key 需要转换的key
+ * @param max 数组的长度，最大数值
+ * @returns 索引值
+ */
+function hashFunc(key: string, max: number): number {
+  //计算HashCode
+  let hashCode = 0
+  const length = key.length
+  for (let i = 0; i < length; i++) {
+    //根据霍纳法则计数hashCode
+    hashCode = 31 * hashCode + key.charCodeAt(i)
+  }
+
+  //求出索引值
+  const index = hashCode % max
+  
+  return index
+}
