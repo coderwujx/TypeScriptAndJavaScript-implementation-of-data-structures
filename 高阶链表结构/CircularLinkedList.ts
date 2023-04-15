f9a6a65aaf8bf32ae277ffa4cfa627d60ce3aa53
WuJXcoder <coderwujx@gmail.com>
2023-04-15 09:24:41 +0800

    循环链表封装insert方法

--- 高阶链表结构/CircularLinkedList.ts
+++ 高阶链表结构/CircularLinkedList.ts
@@ -5,4 +5,14 @@ class CircularLinkedList<T> extends LinkedList<T> {
     //拿到最后一个节点，指向第一个节点
     this.tail!.next = this.head
   }
+
+  //插入节点
+  insert(value: T, position: number): boolean {
+    const isSuccess = super.insert(value, position)
+    if (isSuccess && (position === this.length - 1 || position === 0)) {
+      this.tail!.next = this.head
+    }
+
+    return isSuccess
+  }
 }
